5cedd1e9a7048b2b6fe3343c68dd974369ccb934
Austin Beaty <austin@beaty.tech>
2020-02-10 18:22:25 -0500

    Referenced all -T terminal commands in fbi to settings.json

--- canvas_web_files/var/www/html/pictureFrame.sh
+++ canvas_web_files/var/www/html/pictureFrame.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 TIMER="$(jq '.delay' /var/www/html/settings.json)"
 ROTATINGSTATUS="$(jq -r '.rotating' /var/www/html/settings.json)"
+TERMINAL="$(jq -r '.terminal' /var/www/html/settings.json)"
 
 sudo killall fbi
 
 if [ $ROTATINGSTATUS = true ]; then
-	sudo fbi -noverbose -m 640x480 -a -u -t $TIMER -T 2 /home/pi/Pictures/*
+	sudo fbi -noverbose -m 640x480 -a -u -t $TIMER -T $TERMINAL /home/pi/Pictures/*
 	echo $TIMER
+	echo $TERMINAL
 else
 	echo "The value of the rotating status is" $ROTATINGSTATUS
-	sudo fbi -noverbose -a -T 2 $ROTATINGSTATUS
+	sudo fbi -noverbose -a -T $TERMINAL $ROTATINGSTATUS
+	echo $TERMINAL
 	echo Input received -$1- | wall
 fi
--- canvas_web_files/var/www/html/setImage.sh
+++ canvas_web_files/var/www/html/setImage.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+TERMINAL="$(jq -r '.terminal' /var/www/html/settings.json)"
+
+
 picturePath=$1
 
 sudo killall fbi
 
-sudo fbi -noverbose -a -T 2 $1
+sudo fbi -noverbose -a -T $TERMINAL $1
 
 echo Input received -$1- | wall
